2baf88d2121f22a518c35bc0b157639d3d66bd78
Manoj Singh Dhami <manusinghdhami@gmail.com>
2026-09-18 15:39:47 +0530

    update

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,7 +11,7 @@ public class App {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Java CI/CD on EKS via ArgoCD! Build v7:latest";
+        return "Hello from Java CI/CD on EKS via ArgoCD! Build v8:latest";
     }
 
     public static void main(String[] args) {
